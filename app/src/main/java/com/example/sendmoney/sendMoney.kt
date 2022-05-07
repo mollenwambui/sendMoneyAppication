2e859bac14902438c98d36b624fb536d56f95bc7
mollenwambui <mollenwambui@gmail.com>
2022-05-07 14:06:41 +0300

    changes

--- app/src/main/java/com/example/sendmoney/sendMoney.kt
+++ app/src/main/java/com/example/sendmoney/sendMoney.kt
@@ -1,11 +1,20 @@
 package com.example.sendmoney
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class sendmoney : AppCompatActivity() {
+    lateinit var btncancel: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_send_money)
+
+        btncancel = findViewById(R.id.btncancel)
+        btncancel.setOnClickListener {
+            val intent = Intent(this, sendmoney::class.java)
+            startActivity(intent)
+        }
     }
 }
